16dfab00f8550147a6ce05705b8fe2e103ba0001
emon5369 <emon5369@gmail.com>
2024-09-30 19:39:08 +0600

    design updated

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -87,9 +87,9 @@ const Room = () => {
   return (
     <div className='w-full h-full flex justify-center'>
       <div className='w-11/12 h-full lg:w-2/4'>
-        <div className=" pl-2 h-[10%] flex justify-between items-center">
+        <div className=" pl-2 h-[7%] lg:h-[10%] flex justify-between items-center">
           <h1 className='text-xl lg:text-2xl text-gray-400'>
-            Welcome <span className='text-green-500'>{user.name}</span>!
+            Welcome <span className='bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text font-semibold'>{user.name}</span>!
           </h1>
           <button
             className='bg-slate-700 p-2 m-1 rounded-lg hover:bg-red-700 transition duration-200'
@@ -98,21 +98,21 @@ const Room = () => {
             Logout
           </button>
         </div>
-        <div className='h-[90%] relative p-5 lg:p-9 border border-slate-800'>
+        <div className='h-[93%] lg:h-[90%] relative p-5 lg:p-9 border border-slate-800'>
           <div className='overflow-y-auto h-full pb-14 lg:pb-10 no-scrollbar'>
             {messages.map(message => (
               <div key={message.$id} className=''>
                 <div className='flex justify-between pl-1'>
                   <p className='text-slate-200'>
-                    {message.username ? <span>{message.username}</span> : <span>Anonymous</span>}
+                    {message.username ? <span className='font-sans'>{message.username}</span> : <span className='font-sans'>Anonymous</span>}
                   </p>
-                  <span className='text-slate-400 text-xs p-1'>
+                  <span className='text-slate-400 text-xs p-1 font-sans'>
                     {new Date(message.$createdAt).toLocaleString()}
                   </span>
                 </div>
                 <div className='mb-2 gap-2 flex justify-between'>
                   <span
-                    className={`px-2 lg:px-3.5 p-2 lg:p-2.5 rounded-2xl text-xl 
+                    className={`px-3 py-2 rounded-2xl text-lg 
                       ${message.user_id === user.$id ?
                         'border-2 border-blue-600 text-white' :
                         'bg-blue-600 text-white'
@@ -134,7 +134,7 @@ const Room = () => {
             <div ref={messagesEndRef} />
           </div>
           <form onSubmit={handleSubmit}>
-            <div className="flex absolute bottom-0 left-0 px-5 lg:px-8 pt-2 pb-5 gap-1 w-full">
+            <div className="flex absolute bottom-0 left-0 px-5 lg:px-8 pt-2 pb-4 gap-1 w-full border-b border-slate-800">
               <input
                 className='w-10/12 bg-white text-black text-xl px-5 py-3 rounded-3xl outline-none'
                 type="text"
